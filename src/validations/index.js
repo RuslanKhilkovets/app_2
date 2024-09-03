c48e083aa04f8fe8f06ef9a7b0a6b8df8684e89a
ww8-mac-pro-001 <web.west8@gmail.com>
2024-09-03 13:00:43 +0300

    deleted unused validations

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,4 +1,3 @@
 export {default as loginSchema} from './loginSchema';
 export {default as registerSchema} from './registerSchema';
-export {default as createPostSchema} from './createPostSchema';
 export {default as getSchemeByScreenType} from './changePersonalDataSchema';
