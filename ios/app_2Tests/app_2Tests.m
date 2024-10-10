d266de652285a3fd669b0b262e35821ea98bef4e
ww8-mac-pro-001 <web.west8@gmail.com>
2024-10-10 17:20:39 +0300

    renamed app

--- ios/app_2Tests/app_2Tests.m
+++ ios/app_2Tests/app_2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface app_2Tests : XCTestCase
+@interface eZnaxidkaTests : XCTestCase
 
 @end
 
-@implementation app_2Tests
+@implementation eZnaxidkaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
